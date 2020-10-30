d13007da0e516ed49dfb82fe46c90533f14e9f70
EFdez <escarlafdez@gmail.com>
2020-10-30 21:33:49 +0100

    added Nosotros

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from '../components/layout'
 import ImagenHotel from '../components/imagenHotel'
-import ContenidoInicio from '../components/inicio'
+import ContenidoInicio from '../components/contenidoInicio'
 
 
 const IndexPage = () => (
